8a4f83425b2ca531b518d1a83811f14f2c31d8b6
Eva Ntourou <evedour@gmail.com>
2021-04-20 13:07:37 +0300

    edit MainActivity.java

--- app/src/main/java/com/example/ecoway/MainActivity.java
+++ app/src/main/java/com/example/ecoway/MainActivity.java
@@ -8,14 +8,13 @@ import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
-    ListView simpleList;
-    String[] vehicle_List = {"Ποδήλατα", "Πατίνια", "Ηλεκτρικά Ποδήλατα"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] vehicle_List = {"Ποδήλατα", "Πατίνια", "Ηλεκτρικά Ποδήλατα"};
 
-        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ListView simpleList = (ListView)findViewById(R.id.simpleListView);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, vehicle_List);
         simpleList.setAdapter(arrayAdapter);
     }
