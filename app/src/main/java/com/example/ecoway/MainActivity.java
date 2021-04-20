41f60bf10835ee1464b635c5d2948c43fbb6c424
Unavailable <konsti0114@gmail.com>
2021-04-20 20:17:12 +0300

    update

--- app/src/main/java/com/example/ecoway/MainActivity.java
+++ app/src/main/java/com/example/ecoway/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         String[] vehicle_List = {"Ποδήλατα", "Πατίνια", "Ηλεκτρικά Ποδήλατα"};
     //TODO: Η λίστα δε φορτωνει και κάνει το app να κρασαρει (δεν εχει θεμα το activity_main.xml αλλα η java)
+        //i wanna cry
         //vehicleList = (ListView)findViewById(R.id.vehiclesList);
         //ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, vehicle_List);
         //vehicleList.setAdapter(arrayAdapter);
