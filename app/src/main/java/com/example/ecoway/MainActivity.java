1747233df9aef8d73a790f8812989e54a18db538
Eva Ntourou <evedour@gmail.com>
2021-04-20 12:57:35 +0300

    COMMITCOMMITCOMMIT

--- app/src/main/java/com/example/ecoway/MainActivity.java
+++ app/src/main/java/com/example/ecoway/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.ecoway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView simpleList;
+    String[] vehicle_List = {"Ποδήλατα", "Πατίνια", "Ηλεκτρικά Ποδήλατα"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        simpleList = (ListView)findViewById(R.id.simpleListView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, vehicle_List);
+        simpleList.setAdapter(arrayAdapter);
     }
+
+
 }
