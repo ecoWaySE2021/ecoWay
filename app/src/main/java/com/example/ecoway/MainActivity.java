90a63362cb33adbc38e1f07788cec1d5cdf7f67a
Eva Ntourou <evedour@gmail.com>
2021-05-14 15:13:24 +0300

    prettied up comments

--- app/src/main/java/com/example/ecoway/MainActivity.java
+++ app/src/main/java/com/example/ecoway/MainActivity.java
@@ -14,13 +14,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Μεχρι να συνδέσουμε τα activities, καντε uncomment τη γραμμή 18 για το σταθμό και την 19 για το προφιλ
+        // Μεχρι να συνδέσουμε τα activities, καντε uncomment τη γραμμή 18 για το σταθμό
+            // και την 19 για το προφιλ
         setContentView(R.layout.activity_main);
         // setContentView(R.layout.profile);
 
         vehicleList = (ListView)findViewById(R.id.vehiclesList);
         //arrayAdapter = new ArrayAdapter<String>(this,
-          //      R.layout.activity_main, S1.vehicle_List);
+            // R.layout.activity_main, S1.vehicle_List);
 
         //TODO: Η λίστα δε φορτωνει και κάνει το app να κρασαρει
         // (δεν εχει θεμα το activity_main.xml αλλα η java )
