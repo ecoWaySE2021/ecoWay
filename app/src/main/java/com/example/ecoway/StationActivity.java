1d80ea15244ed269f498e2bd072c6580c84bb53a
Eva Ntourou <evedour@gmail.com>
2021-05-30 20:56:55 +0300

    χρηστο μην αλλαξεις τιποτα θα σε ΣΚΟΤΩΣΩ

--- app/src/main/java/com/example/ecoway/StationActivity.java
+++ app/src/main/java/com/example/ecoway/StationActivity.java
@@ -16,10 +16,16 @@ public class StationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         // Μεχρι να συνδέσουμε τα activities, καντε uncomment τις γραμμές που δε θελετε
         // setContentView(R.layout.activity_station);
-        //setContentView(R.layout.profile);
-         setContentView(R.layout.rewards);
+        setContentView(R.layout.profile);
+         //setContentView(R.layout.rewards);
         //setContentView(R.layout.available_shops);
         //setContentView(R.layout.payment_screen);
+        //setContentView(R.layout.suggested_routes);
+        //TODO suggested routes
+        //setContentView(R.layout.vehicle_selection);
+        //setContentView(R.layout.shop_info);
+
+
 
         vehicleList = (ListView)findViewById(R.id.vehiclesList);
         //arrayAdapter = new ArrayAdapter<String>(this,
